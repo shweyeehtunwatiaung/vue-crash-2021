c73cdc05ccf49962249393d9f416edf664f78833
shwe yee <shweyeehtunwatiaung@gmail.com>
2021-06-11 19:04:33 +0630

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+ <button class="btn" @click="onClick()" :style="{background:color}"> {{ text }} </button>
+</template>
+
+<script>
+export default {
+ name:"Button",
+ props:{
+  text:String,
+  color:String
+ },
+ methods:{
+  onClick(){
+   console.log("click");
+  }
+ }
+}
+</script>
